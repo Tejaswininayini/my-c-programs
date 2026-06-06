9cf5fc719d768a332976adc837170a9578f19f92
Tejaswininayini <nayinitejaswi@gmail.com>
2026-06-06 10:01:41 +0530

    Create empty_history.c

---
+++ empty_history.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+
+char** solution(const char* line, int* out_size) {
+    int len = strlen(line);
+    char* copy = (char*)malloc(len + 1);
+    strcpy(copy, line);
+
+    int top = 0;
+
+    char* token = strtok(copy, ",");
+    while (token != NULL) {
+        if (strcmp(token, "BACK") == 0) {
+            if (top > 0) top--;
+        } else if (strcmp(token, "PEEK") == 0) {
+            // do nothing
+        } else {
+            top++;
+        }
+        token = strtok(NULL, ",");
+    }
+
+    char** result = (char*)malloc(sizeof(char));
+    if (top == 0) {
+        result[0] = (char*)malloc(6);
+        strcpy(result[0], "EMPTY");
+    } else {
+        result[0] = (char*)malloc(10);
+        strcpy(result[0], "NOT EMPTY");
+    }
+    *out_size = 1;
+    free(copy);
+    return result;
+}
